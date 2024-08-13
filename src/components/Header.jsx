d720a3952a7f5aafbe1f776535bbc99be080f729
SNEHA-MKK <sneha.mohandas.kk@gmail.com>
2024-08-13 11:41:19 +0530

    header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,123 @@
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
-import Nav from 'react-bootstrap/Nav';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import React from 'react'
+// import Nav from 'react-bootstrap/Nav';
+// import Container from 'react-bootstrap/Container';
+// import { faMoneyBillTransfer, faCircleQuestion, faCircleUser, faHouse, faPowerOff } from '@fortawesome/free-solid-svg-icons'
+// import Navbar from 'react-bootstrap/Navbar';
+// import { Link } from 'react-router-dom';
+
+
+// function Header() {
+
+//   return (
+//     <>
+//       <Navbar expand="lg" className="bg-primary p-0">
+//         <Container style={{height:"70px"}} >
+//           <Navbar.Brand>
+//           <Link to={'/home'} style={{ textDecoration: "none" }}>
+//             <span style={{ color: 'white',fontSize:"30px" }}><b>Expense Tracker</b></span> </Link>
+//           <Navbar.Toggle aria-controls="basic-navbar-nav" /></Navbar.Brand>
+//           <Navbar.Collapse id="basic-navbar-nav">
+//             <Nav className="ms-auto">
+//               <Link to={'/income'} style={{ textDecoration: 'none' }}>
+//                 <h5 className='text-light mt-2 me-3' >Income</h5></Link>
+
+//               <Link to={'/expenses'} style={{ textDecoration: 'none' }}>
+//                 <h5 className='text-light mt-2 ms-2 me-3' href="#link">Expense</h5></Link>
+
+//               <Link to={'/report'} style={{ textDecoration: 'none' }}>
+//                 <h5 className='text-light mt-2 ms-2 me-2' href="#link">Report</h5></Link>
+
+//               <Nav.Link className='text-light' href=""><FontAwesomeIcon icon={faCircleQuestion} className='ms-2' size='xl' title="Help" /></Nav.Link>
+
+//               <Link to={'/home'} style={{ textDecoration: 'none' }}>
+//                 <Nav.Link className='text-light' href='#home' ><FontAwesomeIcon icon={faHouse} className='ms-3' size='xl' title="Home" /></Nav.Link></Link>
+
+//               <Nav.Link className='text-light' ><FontAwesomeIcon icon={faCircleUser} className='ms-3' size='xl' title="Profile" /></Nav.Link>
+
+//                  <Link to={'/'} style={{ textDecoration: 'none' }}>
+//                 <Nav.Link className='text-light' href='#home' ><FontAwesomeIcon icon={faPowerOff} className='ms-3' size='xl' title='Logout' /></Nav.Link></Link>
+
+
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+
+//     </>
+//   )
+// }
+
+// export default Header/
+
+
+
+
+import React from 'react';
 import Container from 'react-bootstrap/Container';
-import { faMoneyBillTransfer, faCircleQuestion, faCircleUser, faHouse, faPowerOff } from '@fortawesome/free-solid-svg-icons'
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleQuestion, faCircleUser, faHouse, faMoneyBill, faPowerOff } from '@fortawesome/free-solid-svg-icons';
+
+
 
 
 function Header() {
-  
   return (
     <>
-      <Navbar expand="lg" className="bg-primary">
-        <Container style={{height:"70px"}} >
-          <Navbar.Brand>
-          <Link to={'/home'} style={{ textDecoration: "none" }}>
-            <FontAwesomeIcon className='me-3' icon={faMoneyBillTransfer} size='xl' style={{ color: "white", }} /><span style={{ color: 'white',fontSize:"30px" }}><b>Expense Tracker</b></span> </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" /></Navbar.Brand>
+
+
+      <Navbar expand="lg" className="mx-3 rounded shadow navbar  mb-1 mt-3  " >
+        <Container fluid className='justify-content-between align-items-center'>
+          <Navbar.Brand className='fw-bolder text-info'>
+            <Link to={'/home'} style={{ textDecoration: "none" }}>
+              <FontAwesomeIcon icon={faMoneyBill} />  <span style={{ color: 'black', fontSize: "30px" }}><b>Expense Tracker</b></span> </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Link to={'/income'} style={{ textDecoration: 'none' }}>
-                <h5 className='text-light mt-2 me-3' >Income</h5></Link>
+                <h5 className='text-dark mt-2 me-3 ms-2' >Income</h5></Link>
 
               <Link to={'/expenses'} style={{ textDecoration: 'none' }}>
-                <h5 className='text-light mt-2 ms-2 me-3' href="#link">Expense</h5></Link>
+                <h5 className='text-dark mt-2 ms-2 me-3' href="#link">Expense</h5></Link>
 
               <Link to={'/report'} style={{ textDecoration: 'none' }}>
-                <h5 className='text-light mt-2 ms-2 me-2' href="#link">Report</h5></Link>
+                <h5 className='text-dark mt-2 ms-2 me-2' href="#link">Report</h5></Link>
 
-              <Nav.Link className='text-light' href=""><FontAwesomeIcon icon={faCircleQuestion} className='ms-2' size='xl' title="Help" /></Nav.Link>
+          
 
               <Link to={'/home'} style={{ textDecoration: 'none' }}>
-                <Nav.Link className='text-light' href='#home' ><FontAwesomeIcon icon={faHouse} className='ms-3' size='xl' title="Home" /></Nav.Link></Link>
+                <Nav.Link className='text-dark' href='#home' ><FontAwesomeIcon icon={faHouse} className='ms-3' size='xl' title="Home" /></Nav.Link></Link>
 
-              <Nav.Link className='text-light' ><FontAwesomeIcon icon={faCircleUser} className='ms-3' size='xl' title="Profile" /></Nav.Link>
+              {/* <Link to={'/'} style={{ textDecoration: 'none' }}>
+                <Nav.Link className='text-dark' href='#home' ><FontAwesomeIcon icon={faPowerOff} className='ms-3' size='xl' title='Logout' /></Nav.Link></Link> */}
 
-                 <Link to={'/'} style={{ textDecoration: 'none' }}>
-                <Nav.Link className='text-light' href='#home' ><FontAwesomeIcon icon={faPowerOff} className='ms-3' size='xl' title='Logout' /></Nav.Link></Link>
 
+            </Nav>
+            <Nav className='ms-auto jusify-content-between'>
+
+              {/* <Link to={'/register'} >
+                <button className='btn btn-info text-light rounded-5 mx-lg-2 my-2 my-lg-0'>
+                  SignUp
+                </button>
+              </Link> */}
+              <Link to={'/'}>
+                <button className='btn btn-info text-light rounded-5 mx-lg-2 my-2 my-lg-0'>
+                  LogOut
+                </button>
+              </Link>
 
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
+
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,58 +1,3 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-
-// function Landingpage() {
-//   return (
-//     <>
-//      <section >
-//           <div className="row my-5 p-5">
-//             <div className="col-md-1"></div>
-//             <div className="col-md-5">
-//                 <h4  style={{overflowY:"hidden",color:"lightblue"}}>What is an Expense tracker?</h4>
-//                 <p style={{textAlign:"justify"}}> Keeping a daily record of your expenses by tracking receipts, invoices, and other outgoing expenses improves the financial health of your budget.Expense tracking means recording all your expenditures so you have a clear and detailed understanding of your budget.This can be achieved with the help of an expense tracker which enables you to monitor how much of your budget has been spent over time, to see how much is remaining and course-correct when necessary. </p>
-//             </div>
-//             <div className="col-md-1"></div>
-//             <div className="col-md-5">
-//                 <img src="https://cdn.dribbble.com/users/827787/screenshots/5930698/digital-strategy.gif" width={'350px'} alt="" />
-//             </div>
-//           </div >
-          
-//      </section>
-//      <hr />
-
-//      <section>
-//         <div className="row my-5">
-//             <h3 className=' text-center text-warning'>Wanna find Your Expenses?</h3>
-//             <div className="col-md-2"></div>
-//             <div className="col-md-5 mt-5 ">
-//                 <img src="https://cdn.dribbble.com/users/513906/screenshots/3986167/moneyslides_800x600.gif" width={'350px'} alt="" />
-//             </div>
-//             <div className="col-md-5 mt-4 pt-5">
-//                <h6>Add Credit money : 
-//                 <Link to={'/income'}>
-//                <button style={{borderRadius:"8px",color:"white"}} className='ms-2 p-2 bg-warning'>Add Income</button>
-//                </Link> </h6>
-//                <h6 className='mt-3'>Add Your Expenses :
-//                 <Link to={'/expenses'}><button style={{borderRadius:"8px",color:"white"}} className='ms-1 p-2 bg-warning border-outline-transparent'>Add Expense</button>
-//                 </Link>
-//                  </h6>
-//                <h6 className='mt-3'>View Monthly Report : 
-//                <Link to={'/report'}><button style={{borderRadius:"8px",color:"white",width:"100px"}} className='ms-1 p-2 bg-warning'>Report</button> </Link>
-//                </h6>
-//             </div>
-//             <div className='col-md-1'></div>
-           
-//         </div>
-//      </section>
-//      <hr />
-//     </>
-//   )
-// }
-
-// export default Landingpage
-
-
-
 
 
  import { faSackDollar,faClipboard ,faChartSimple} from '@fortawesome/free-solid-svg-icons'
@@ -106,11 +51,11 @@ function Landingpage() {
          
             <h6 className='mt-3 mx-4'>
               
-                <button onClick={()=>incomePage()} variant="outline-light" style={{ borderRadius: "8px", color: "white", border: 'none' }} className='ms-2 p-2 bg-success w-50'><FontAwesomeIcon icon={faSackDollar} className='me-2' />ADD INCOME</button> <br />
+               <Link to={'/income'}> <button  variant="outline-light" style={{ borderRadius: "8px", color: "white", border: 'none' }} className='ms-2 p-2 bg-success w-50'><FontAwesomeIcon icon={faSackDollar} className='me-2' />ADD INCOME</button> </Link><br />
               </h6>
 
             <h6 className='mt-3 mx-4'>
-              <button onClick={()=>expensePage()} style={{ borderRadius: "8px", color: "white", border:'none' }} className='ms-1 w-50  p-2 bg-danger '><FontAwesomeIcon className='me-2' icon={faClipboard} />ADD EXPENSES</button>
+              <Link to={'/expenses'}><button style={{ borderRadius: "8px", color: "white", border:'none' }} className='ms-1 w-50  p-2 bg-danger '><FontAwesomeIcon className='me-2' icon={faClipboard} />ADD EXPENSES</button></Link>
             </h6>
 
             <h6 className='mt-3 mx-4'>
